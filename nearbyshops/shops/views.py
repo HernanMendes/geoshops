ab4b31c43711e1509e88ab53830ae2922f1cf7b9
HernanMendes <hmendesgouveia@gmail.com>
2023-05-27 13:04:44 -0300

    feat: display nearby shops

--- nearbyshops/shops/views.py
+++ nearbyshops/shops/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
 
-# Create your views here.
+
+longitude = -58.38162
+latitude = -34.60376
+
+user_location = Point(longitude, latitude, srid=4326)
+
+
+class Home(ListView):
+    model = Shop
+    context_object_name = 'shops'
+    queryset = Shop.objects.annotate(distance=Distance('location', user_location)
+                                     ).order_by('distance')[0:6]
+    template_name = 'shops/index.html'
